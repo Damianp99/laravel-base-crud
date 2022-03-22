6e15f99b67884d04c0714e723f42684e6ac22f1f
Damianp99 <damianperez200199@gmail.com>
2022-03-23 00:05:58 +0100

    changed html of index page

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,17 +1,35 @@
 @extends('layouts.main')
 @section('title',' Indice')
 @section('content')
-@forelse ($comics as $comic)
-<ul>
-    <li>{{$comic->title}}</li>
-    <li>{{$comic->description}}</li>
-    <li><img src="{{$comic->thumb}}" alt="{{$comic->title}}"></li>
-    <li>{{$comic->price}}€</li>
-    <li>{{$comic->series}}</li>
-    <li>{{$comic->sale_date}}</li>
-    <li>{{$comic->type}}</li></ul>
-@empty
-    <p>Non c'è alcun fumetto .</p>
-@endforelse
+<section class="container">
+    <div class="d-flex flex-column align-items-center p-4 bg-black rounded">
+        <h1 class="text-primary text-shadow ">Ecco l'indice dei fumetti che possediamo</h1>
+        <a href="{{route('comics.create')}}" class="btn btn-primary my-2">Aggiungi un fumetto</a>
+    </div>
+    <div class="row">
+        @forelse ($comics as $comic)
+            <div class="col-3 p-3">
+                <div class="card my-3" style="width: 18rem;">
+                    <img src="{{$comic->thumb}}" class="card-img-top" alt="{{$comic->title}}">
+                    <div class="card-body">
+                      <h5 class="card-title">{{$comic->title}}</h5>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item"><strong>Prezzo: </strong>{{$comic->price}}€</li>
+                      <li class="list-group-item"><strong>Serie: </strong>{{$comic->series}}</li>
+                      <li class="list-group-item"><strong>Data: </strong>{{$comic->sale_date}}</li>
+                      <li class="list-group-item"><strong>Tipologia: </strong>{{$comic->type}}</li>
+                    </ul>
+                    <div class="card-body">
+                      <a href="{{route('comics.show',$comic->id)}}" class="card-link">Più informazioni</a>
+                    </div>
+                </div>
+            </div>
+        @empty
+            <p>Non c'è alcun fumetto .</p>
+        @endforelse    
+    </div>
+</section>
+
 
 @endsection
