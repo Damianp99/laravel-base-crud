3370350e83c27fb5a89f46d43637cb91aee67a14
Damianp99 <damianperez200199@gmail.com>
2022-03-23 00:05:18 +0100

    added logo

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,7 @@
-<header class="d-flex justify-content-center align-items-center ">
+<header class="bg-black d-flex justify-content-center align-items-center ">
+    <img src="{{asset('images/dc-logo.png')}}" class="py-2" alt="Logo DC Comics">
     <nav class="nav">
-        <a class="nav-link h2" href="{{route('home')}}">Home</a>
-        <a class="nav-link h2" href="{{route('comics.index')}}">Index</a>
+        <a class="nav-link h2 text-shadow " href="{{route('home')}}">Home</a>
+        <a class="nav-link h2 text-shadow " href="{{route('comics.index')}}">Index</a>
       </nav>
 </header>
