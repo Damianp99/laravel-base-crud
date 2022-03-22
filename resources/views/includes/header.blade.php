45463f3d63956c5a45d606b3e0d035b5456022c9
Damianp99 <damianperez200199@gmail.com>
2022-03-22 17:20:11 +0100

    added routes for home and index pages

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,3 +1,6 @@
-<header>
-    
+<header class="d-flex justify-content-center align-items-center ">
+    <nav class="nav">
+        <a class="nav-link h2" href="{{route('home')}}">Home</a>
+        <a class="nav-link h2" href="{{route('comics')}}">Index</a>
+      </nav>
 </header>
