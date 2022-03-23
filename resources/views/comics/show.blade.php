7c3c04aa60e332f754a6626c3164dd4a0fe096db
Damianp99 <damianperez200199@gmail.com>
2022-03-23 17:32:13 +0100

    added edit link to show page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,12 +2,15 @@
 @section('title',' Fumetto')
 @section('content')
 <div class="container p-5">
-    <div class="row bg-secondary d-flex  p-5 bg-dark">
+    <div class="row  d-flex  p-5 bg-dark">
         <div class="col-6 ">
              <img src="{{$comic->thumb}}" class="img-comic" alt="{{$comic->title}}">
         </div>
         <div class="col-6 d-flex flex-column text-white">
-            <h5><strong>Titolo: </strong>{{$comic->title}}</h5>
+            <div class="d-flex justify-content-between">
+                <h5><strong>Titolo: </strong>{{$comic->title}}</h5>
+                <a href="{{route('comics.edit',$comic->id)}}" class="card-link text-white shadow-azure"><i class="fa-solid fa-pen"></i></a>
+            </div>
             <ul class="list-unstyled">
               <li><strong>Prezzo: </strong>{{$comic->price}}€  </li>
               <li><strong>Serie: </strong>{{$comic->series}}</li>
